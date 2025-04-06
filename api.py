ebf6ddb017c009b8c6522277ea78fd441ea9f5bb
Moirius <biotteaumarius@gmail.com>
2025-04-07 01:23:04 +0200

    ✅ fix: autorisation CORS pour frontend local et Netlify

--- api.py
+++ api.py
@@ -25,7 +25,11 @@ app = FastAPI()
 # CORS pour frontend local
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Remplace par ton domaine si besoin
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://lastation-prod.com/",  # ← ton vrai domaine Netlify ici
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
